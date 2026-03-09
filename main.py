38c3cd8ed209a19148eba5783766288138a3f246
LauMontironi <laura.montironi@bootcamp-upgrade.com>
2026-03-09 08:00:19 +0100

    fix: habilitar CORS para frontend Angular

--- main.py
+++ main.py
@@ -10,16 +10,17 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-app.add_middleware( 
+app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:4200",
-        "http://127.0.0.1:4200",
+        "http://127.0.0.1:4200"
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 
 
